4a274181441578b68f629f28ac1ad90b3dc1720e
Namrata7Asati7 <namrataasati3@gmail.com>
2024-11-05 17:11:27 +0530

    commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from app import create_app
+from app import app
 from urllib.parse import quote as url_quote
 
 
-app = create_app()
+app = app()
 
 if __name__ == '__main__':
     app.run(debug=True)
